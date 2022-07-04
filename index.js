cb11039c2a0487aa2662fd5c532615c23aac8c6f
Jesus Mendoza <jesus_mendoza1881@yahoo.com>
2022-07-04 14:49:21 -0700

    done

---
+++ index.js
@@ -0,0 +1,73 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: "input",
+    name: "projectName",
+    message: "What is the name of the project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Give a brief description",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Write the installation process",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What is the usage of this project?",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Chose the appropriate license for this project: ",
+    choices: ["Apache", "Boost", "GNU", "ISC", "MIT", "Mozilla", "Open"],
+  },
+  {
+    type: "input",
+    name: "tests",
+    message:
+      "Please enter any testing instructions you would like to provide for this project.",
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "How can users contribute to this project.",
+  },
+
+  {
+    type: "input",
+    name: "username",
+    message: "Enter your Github Username",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter your email",
+  },
+];
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, generateMarkdown(data), function (err) {
+    if (err) {
+      return console.log(err);
+    }
+  });
+}
+// TODO: Create a function to initialize app
+function init() {
+  inquirer.prompt(questions).then((data) => {
+    console.log(JSON.stringify(data, null, " "));
+    writeToFile("README.md", data);
+  });
+}
+// Function call to initialize app
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,57 @@
+// To do: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  
+  const licenseChoice = data.license;
+  
+  console.log(licenseChoice);
+  
+  function renderLicenseBadge() {
+    if (licenseChoice == "Apache") {
+      return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    } else if (licenseChoice == "Boost") {
+      return "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
+    } else if (licenseChoice == "GNU") {
+      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+    } else if (licenseChoice == "ISC") {
+      return "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
+    } else if (licenseChoice == "MIT") {
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    } else if (licenseChoice == "Mozilla") {
+      return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+    } else if (licenseChoice == "Open") {
+      return "[![License: Open Data Commons Attribution](https://img.shields.io/badge/License-ODC_BY-brightgreen.svg)](https://opendatacommons.org/licenses/by/)";
+    } else {
+      return;
+      ("");
+    }
+  }
+
+  return `
+  # ${data.projectName}
+  ## ${data.license}
+  ${renderLicenseBadge()}
+
+  # Description:
+  ${data.description}
+  # Table of Content:
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+# Installation
+${data.installation}
+# Usage
+${data.usage}
+# Contributing
+${data.contributing}
+# Test
+${data.tests}
+# Questions
+Here is the following information to reach the creator:
+- Github Username: [${data.username}](https://github.com/${data.username})
+- Email Address: ${data.email}
+`;
+}
+
+module.exports = generateMarkdown;
